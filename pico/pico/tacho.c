1f51f267da6eea92c6de2f1972024a183e3f9551
svofski <svofski@gmail.com>
2023-07-03 02:23:54 +0400

    detect too fast spin in play mode

--- pico/pico/tacho.c
+++ pico/pico/tacho.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "pico/stdlib.h"
 #include "tacho.h"
 #include "wheel.h"
@@ -38,9 +39,61 @@ int tacho_get_counter()
     return tacho_counter;
 }
 
+// ---- tape rip detection ------
+// detect if takeup reel is moving too fast
+static int last_tc = 0;
+static int toofast_enabled = 0;
+static int toofast[10]; // average
+static int toofast_i = 0; 
+
+void init_toofast(int enabled)
+{
+    toofast_enabled = enabled;
+
+    if (enabled) {
+        for (int i = 0; i < 10; ++i) {
+            toofast[i] = 0;
+            toofast_i = 0;
+        }
+        last_tc = tacho_counter;
+    }
+}
+
+bool check_toofast()
+{
+    if (!toofast_enabled) 
+        return false;
+
+    toofast[toofast_i] = abs(tacho_counter - last_tc);
+    last_tc = tacho_counter;
+
+    toofast_i += 1;
+    if (toofast_i >= 10) {
+        toofast_i = 0;
+    }
+
+    int sum = 0;
+    for (int i = 0; i < 10; ++i) {
+        sum += toofast[i];
+    }
+
+    if (sum > 25) {
+        return true;
+    }
+
+    return false;
+}
+// -----------------------------------
+
 bool autostop_alarm_callback(repeating_timer_t *rt)
 {
     ++autostop_alarm_counter;
+
+    if (check_toofast()) {
+        request_wheel_stop();
+        return false;
+    }
+
     if (autostop_alarm_counter > AUTOSTOP_COUNTER_LIMIT) {
         //printf("AUTOSTOP\n");
         request_wheel_stop();
@@ -57,6 +110,8 @@ void autostop_alarm_start(int check_interval)
     autostop_alarm_stop();
     add_repeating_timer_ms(check_interval, autostop_alarm_callback, 
             /*userdata*/NULL, &autostop_timer);
+
+    init_toofast(check_interval == AUTOSTOP_SLOW);
 }
 
 void autostop_alarm_reset()
--- pico/pico/tacho.h
+++ pico/pico/tacho.h
@@ -2,7 +2,7 @@
 
 #define AUTOSTOP_COUNTER_LIMIT  2
 #define AUTOSTOP_QUICK          50
-#define AUTOSTOP_SLOW           150 // 100 is ok at reel start, but near the end it is slower
+#define AUTOSTOP_SLOW           175 // 100 is ok at reel start, but near the end it is slower
 
 void autostop_alarm_start(int check_interval);
 void autostop_alarm_stop();
