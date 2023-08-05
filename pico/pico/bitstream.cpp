a03f41191bf11dbb04eace8be26490a81d498f23
svofski <svofski@gmail.com>
2023-08-05 04:49:34 +0400

    mfm send-receive on a loopback wire
    wire between gpio 16 and 17
    
    super-naiive rx tolerates slightly faster send, but not slower

--- pico/pico/bitstream.cpp
+++ pico/pico/bitstream.cpp
@@ -10,6 +10,7 @@
 #include <array>
 
 #include "pico/stdlib.h"
+#include "pico/multicore.h"
 #include "hardware/pio.h"
 #include "bitstream.pio.h"
 
@@ -24,6 +25,12 @@ const uint32_t MFM_SYNC = 0x55554489;  // prefixed with leader 01010101...
 
 const char * get_plaintext();
 size_t get_plaintext_size();
+
+
+// can we share one PIO between two cores?
+PIO pio = pio0;
+uint sm_tx = 0, sm_rx = 1;
+
 
 // 8 bits -> 16 mfm-bits, returns last bit 
 uint32_t mfm_encode_twobyte(uint8_t c1, uint8_t c2, uint8_t *prev_bit)
@@ -72,12 +79,12 @@ std::array<uint32_t, 64> buffer;
 
 
 // try running it on core1?
-uint32_t mfm_wait_sync(PIO pio, uint sm_rx, uint16_t sync16, uint max_bits)
+uint32_t mfm_wait_sync(PIO pio, uint sm_rx, uint16_t sync16, int max_bits)
 {
     uint32_t prev = 0;
     int sample_t = 0;
 
-    for(uint i = 0; i < max_bits; ++i) {
+    for (uint i = 0; i < max_bits; ++i) {
         uint32_t cur = pio_sm_get_blocking(pio, sm_rx);
         if (cur != prev) {
             sample_t = 0;
@@ -105,13 +112,14 @@ uint32_t mfm_wait_sync(PIO pio, uint sm_rx, uint16_t sync16, uint max_bits)
     return 0;
 }
 
-#if 0
-uint32_t mfm_recv(PIO pio, uint sm_rx)
+// sample and return 32 mfm-bits
+// return value suitable for decoding with mfm_decode_twobyte()
+uint32_t mfm_recv32(PIO pio, uint sm_rx)
 {
     uint32_t prev = 0;
     int sample_t = 0;
 
-    for(uint i = 0; i < max_bits; ++i) {
+    for (uint n = 0; n < 32;) {
         uint32_t cur = pio_sm_get_blocking(pio, sm_rx);
         if (cur != prev) {
             sample_t = 0;
@@ -123,27 +131,37 @@ uint32_t mfm_recv(PIO pio, uint sm_rx)
             }
         }
 
+        // take sample hopefully in the middle 
         if (sample_t == 4) {
-            // take sample hopefully in the middle 
             mfm_bits = (mfm_bits << 1) | cur;
-            //printf("%x ", mfm_bits);
-            if ((mfm_bits & 0xffff) == sync16) {
-                //printf("HOORAY\n");
-                return mfm_bits;
-            }
+            ++n;
         }
 
         prev = cur;
     }
 
-    return 0;
+    return mfm_bits;
 }
-#endif
+
+void core1_entry()
+{
+    uint32_t rxsync;
+    do {
+        rxsync = mfm_wait_sync(pio, sm_rx, MFM_SYNC_A1, 1024);
+    } while ((rxsync & 0xffff) != MFM_SYNC_A1);
+
+    uint8_t c1, c2;
+    mfm_decode_twobyte(rxsync, &c1, &c2);
+    printf("rxsync=$%x (%02x %02x)\n", rxsync, c1, c2);
+    for(;;) {
+        uint32_t tb = mfm_recv32(pio, sm_rx);
+        mfm_decode_twobyte(tb, &c1, &c2);
+        putchar(c1);
+        putchar(c2);
+    }
+}
 
 void bitstream_test() {
-    PIO pio = pio0;
-    uint sm_tx = 0, sm_rx = 1;
-
     uint offset_tx = pio_add_program(pio, &bitstream_tx_program);
     printf("Transmit program loaded at %d\n", offset_tx);
     uint offset_rx = pio_add_program(pio, &bitstream_rx_program);
@@ -159,47 +177,37 @@ void bitstream_test() {
 
     uint8_t mfm_prev_bit;
 
-    printf("SEND MFM_SYNC\n");
-    pio_sm_clear_fifos(pio, sm_rx);
-    pio_sm_put_blocking(pio, sm_tx, MFM_SYNC);
-
-    uint32_t rxsync = mfm_wait_sync(pio, sm_rx, MFM_SYNC_A1, 1024);
-
-    uint8_t c1, c2;
-    mfm_decode_twobyte(rxsync, &c1, &c2);
-    printf("rxsync=$%x (%02x %02x)\n", rxsync, c1, c2);
-
-    //printf("\n--- buffor ---\n");
-    //for(uint i = 0; i < 64; ++i) {
-    //    printf("%x ", buffer[i]);
-    //}
-    //printf("\n--- buffor ---\n");
-
-    return;
-
-    mfm_prev_bit = 1; // because the last bit in A1 sync is "1"
-
     printf("getting plaintext\n");
     size_t psz = get_plaintext_size();
     const char * plaintext = get_plaintext();
-
     printf("got plaintext\n");
 
     pio_sm_clear_fifos(pio, sm_rx);
 
+    // launch the receiver in core1
+    multicore_launch_core1(core1_entry);
+
+    // wait for the core to launch
+    sleep_ms(10);
+
+    printf("SEND MFM_SYNC\n");
+    pio_sm_put_blocking(pio, sm_tx, MFM_SYNC);
     for(size_t i = 0; i < psz; i += 2) {
-        putchar(plaintext[i]);
-        putchar(plaintext[i + 1]);
         uint32_t mfm_encoded = mfm_encode_twobyte(plaintext[i], plaintext[i + 1],
                 &mfm_prev_bit);
 
         pio_sm_put_blocking(pio, sm_tx, mfm_encoded); // 32*8 = 256 cycles
     }
-    pio_sm_set_enabled(pio, sm_tx, false);
 
+    // let the receiver finish before shutting down
+    sleep_ms(250);
+
+    // shut down core1
+    multicore_reset_core1();
+
+    // shut down PIO
     pio_sm_set_enabled(pio, sm_tx, false);
     pio_sm_set_enabled(pio, sm_rx, false);
-    //void pio_remove_program(PIO pio, const pio_program_t *program, uint loaded_offset);
     pio_remove_program(pio, &bitstream_tx_program, offset_tx);
     pio_remove_program(pio, &bitstream_rx_program, offset_rx);
     pio_clear_instruction_memory(pio);
